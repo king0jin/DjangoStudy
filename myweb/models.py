250343a12f6afb767220948a0fe022553e6b4871
king0jin <et0709@naver.com>
2024-10-30 13:36:31 +0900

    데이터베이스 먼저 작성 - 클래스 매핑

--- myweb/models.py
+++ myweb/models.py
@@ -5,6 +5,7 @@ from enum import unique
 # 데이터베이스에서 사용할 테이블을 클래스로 생성한다
 # 테이블 = 클래스
 
+#1. 클래스먼저 작성하여 데이터베이스와 매핑
 #Owner클래스 생성
 class Owner(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -25,3 +26,11 @@ class Car(models.Model):
     car_number = models.CharField(max_length=128)
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     car_model = models.ForeignKey(Car_Model, on_delete=models.CASCADE)
+
+#2. 데이터베이스에 테이블을 생성해서 클래스와 매핑
+class Item(models.Model):
+    itemid = models.IntegerField(primary_key=True)
+    itemname = models.CharField(max_length=128)
+    price = models.IntegerField()
+    description = models.CharField(max_length=128)
+    pictureurl = models.CharField(max_length=50)
